a983b91cd2d76daab33873c9bb98d7b74bee2593
alex.rdn <cher.alex97@gmail.com>
2020-05-23 13:42:02 +0300

    Moved brain-even logic to games/brain-even.js

---
+++ games/brain-even.js
@@ -0,0 +1,11 @@
+const isEven = (num) => num % 2 === 0;
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max + 1 - min)) + min;
+
+const getQA = () => {
+  const num = getRandomNumber(0, 100);
+  const correctAnswer = (isEven(num)) ? 'yes' : 'no';
+  return [num, correctAnswer];
+};
+
+export default () => ['Answer "yes" if the number is even, otherwise answer "no".', getQA];

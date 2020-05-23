ef8adacbb767b77846f0a63b7410c6c12b7ddb6b
alex.rdn <cher.alex97@gmail.com>
2020-05-23 13:45:33 +0300

    Modified bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 
-import getAnswer from '../src/getAnswer.js';
-import greet from '../src/greet.js';
-import playBrainEven from '../src/playBrainEven.js';
+import playGame from '../src/index.js';
+import brainEven from '../games/brain-even.js';
 
-console.log('Welcome to the Brain Games!');
-
-const userName = getAnswer('May I have your name?');
-
-greet(userName);
-playBrainEven(userName);
+playGame(brainEven());

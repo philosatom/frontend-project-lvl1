886ece6390224002db550a3a280cfe78a88b0f57
alex.rdn <cher.alex97@gmail.com>
2020-05-24 15:34:09 +0300

    Created brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import playGame from '../src/index.js';
+import brainProgression from '../games/brain-progression.js';
+
+playGame(brainProgression());
---
+++ games/brain-progression.js
@@ -0,0 +1,17 @@
+import { getRandomNumbers, getProgression } from '../src/math.js';
+
+const getQA = () => {
+  const [start, step] = getRandomNumbers(0, 100, 2);
+  const [randomIndex] = getRandomNumbers(0, 9, 1);
+  const progression = getProgression(start, 10, step);
+  const hiddenNum = progression[randomIndex];
+
+  progression[randomIndex] = '..';
+
+  const modifiedProgression = progression.join(' ');
+  const correctAnswer = hiddenNum.toString();
+
+  return [modifiedProgression, correctAnswer];
+};
+
+export default () => ['What number is missing in the progression?', getQA];

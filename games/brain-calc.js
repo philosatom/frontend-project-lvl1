6dfe58403dbfcca2ced54c7da8e9e09843954101
alex.rdn <cher.alex97@gmail.com>
2020-05-24 15:26:36 +0300

    Refactoring

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -4,11 +4,10 @@ const getQA = () => {
   const operators = ['+', '-', '*'];
   const [randomIndex] = getRandomNumbers(0, 2, 1);
   const operator = operators[randomIndex];
-
   const [num1, num2] = getRandomNumbers(0, 100, 2);
+  const result = calc(num1, num2, operator);
 
   const expression = `${num1} ${operator} ${num2}`;
-  const result = calc(num1, num2, operator);
   const correctAnswer = result.toString();
 
   return [expression, correctAnswer];
--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -2,9 +2,9 @@ import { getRandomNumbers, getGcd } from '../src/math.js';
 
 const getQA = () => {
   const [num1, num2] = getRandomNumbers(0, 100, 2);
+  const result = getGcd(num1, num2);
 
   const numsStr = `${num1} ${num2}`;
-  const result = getGcd(num1, num2);
   const correctAnswer = result.toString();
 
   return [numsStr, correctAnswer];

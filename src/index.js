c3f3af888cb168bdb04c35713c4997416afeb389
alex.rdn <cher.alex97@gmail.com>
2020-05-27 07:43:24 +0300

    Modified game engine

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
-import getAnswer from './getAnswer.js';
-import greetPlayer from './greet.js';
+import readlineSync from 'readline-sync';
 
-const showFinalMessage = (isWinner, name) => {
-  const message = (isWinner) ? 'Congratulations' : 'Let\'s try again';
-  const finalMessage = `${message}, ${name}!`;
+export default (game) => {
+  const [rules, getQuestionAndAnswer] = game();
 
-  console.log(`${finalMessage}`);
-};
-
-export default ([rules, getQA]) => {
   console.log('Welcome to the Brain Games!');
 
-  const playerName = getAnswer('May I have your name?');
-  greetPlayer(playerName);
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
 
   console.log(rules);
-  let isWinner = true;
 
   for (let i = 0; i < 3; i += 1) {
-    const [questionText, correctAnswer] = getQA();
+    const [questionText, correctAnswer] = getQuestionAndAnswer();
 
     console.log(`Question: ${questionText}`);
-    const playerAnswer = getAnswer('Your answer:');
+    const playerAnswer = readlineSync.question('Your answer: ');
 
     if (playerAnswer !== correctAnswer) {
       console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
-      isWinner = false;
-      break;
+      console.log(`Let's try again, ${playerName}`);
+      return;
     }
     console.log('Correct!');
   }
 
-  showFinalMessage(isWinner, playerName);
+  console.log(`Congratulations, ${playerName}`);
 };

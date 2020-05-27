1ce7f581ad508285cfd8ec65e5b4afe47835be9d
alex.rdn <cher.alex97@gmail.com>
2020-05-27 07:11:52 +0300

    Removed bin/brain-games.js

--- Makefile
+++ Makefile
@@ -7,9 +7,6 @@ publish:
 lint:
 	npx eslint .
 
-brain-games:
-	node bin/brain-games.js
-
 brain-even:
 	node bin/brain-even.js
 
--- bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import getAnswer from '../src/getAnswer.js';
-import greet from '../src/greet.js';
-
-console.log('Welcome to the Brain Games!');
-greet(getAnswer('May I have your name?'));

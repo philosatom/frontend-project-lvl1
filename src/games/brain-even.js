0080e7a8decb404ccce9edb7a70a597cd7e55c4f
alex.rdn <cher.alex97@gmail.com>
2020-05-27 08:34:56 +0300

    Modified src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,9 +1,9 @@
-import { getRandomNumbers } from '../math.js';
+import getRandomNumber from '../getRandomNumber.js';
 
 const isEven = (num) => num % 2 === 0;
 
 const getQuestionAndAnswer = () => {
-  const [num] = getRandomNumbers(0, 100, 1);
+  const num = getRandomNumber(0, 100);
   const correctAnswer = (isEven(num)) ? 'yes' : 'no';
 
   return [num, correctAnswer];

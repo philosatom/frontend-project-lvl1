df714e2fb731974f558f4df5da0ad64135353d90
alex.rdn <cher.alex97@gmail.com>
2020-05-30 14:37:02 +0300

    brain-calc refactoring

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -13,17 +13,27 @@ const calculate = (operand1, operand2, operator) => {
   }
 };
 
-const getQuestionAndAnswer = () => {
-  const operators = ['+', '-', '*'];
-  const randomIndex = getRandomNumber(0, 2);
-  const operator = operators[randomIndex];
-  const [num1, num2] = times(2, getRandomNumber, 0, 100);
-  const result = calculate(num1, num2, operator);
+const operators = ['+', '-', '*'];
+const lastOperatorIndex = operators.length - 1;
+const getRandomOperator = () => {
+  const operatorIndex = getRandomNumber(0, lastOperatorIndex);
+  const operator = operators[operatorIndex];
 
-  const expression = `${num1} ${operator} ${num2}`;
-  const correctAnswer = result.toString();
-
-  return [expression, correctAnswer];
+  return operator;
 };
 
-export default () => ['What is the result of the expression?', getQuestionAndAnswer];
+const description = 'What is the result of the expression?';
+
+const getQuestionAndAnswer = () => {
+  const operator = getRandomOperator();
+  const operandsCount = 2;
+  const [operand1, operand2] = times(operandsCount, getRandomNumber, 0, 100);
+  const result = calculate(operand1, operand2, operator);
+
+  const question = `${operand1} ${operator} ${operand2}`;
+  const correctAnswer = result.toString();
+
+  return [question, correctAnswer];
+};
+
+export default () => [description, getQuestionAndAnswer];

06d65163b34e9930da08301c36fee0c97d5efeed
alex.rdn <cher.alex97@gmail.com>
2020-06-02 21:38:37 +0300

    brain-prime refactoring

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,10 +2,9 @@ import { getRandomNumber } from '../utils.js';
 
 const isPrime = (number) => {
   if (number <= 1) return false;
+  if (number % 2 === 0) return false;
 
   const maxDivider = Math.sqrt(number);
-
-  if (number % 2 === 0) return false;
 
   for (let divider = 3; divider <= maxDivider; divider += 2) {
     if (number % divider === 0) {
